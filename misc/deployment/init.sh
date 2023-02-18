c8da699556f5d9253b846a690f2ca6d5a044d248
trannamtrung1st <trannamtrung1st@gmail.com>
2023-02-18 23:35:19 +0700

    Update template

--- misc/deployment/init.sh
+++ misc/deployment/init.sh
@@ -19,6 +19,8 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
+cd /home/defaultuser
+
 git clone https://github.com/trannamtrung1st/DLock.git
 
 cd DLock
